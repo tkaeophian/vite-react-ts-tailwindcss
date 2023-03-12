987294311cbd418a6ce72db64136120e924cecd1
tee <tkaeophian@gmail.com>
2023-03-12 15:17:00 +0000

    adding translation

--- src/features/auth/api/me.ts
+++
@@ -1,5 +0,0 @@
-import { AuthUser } from '../types';
-
-import { axios } from '@/lib/axios';
-
-export const me = (): Promise<AuthUser> => axios.get('/auth/me');
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import './index.css';
+import './i18n';
 
 import App from './App';
-import './index.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
